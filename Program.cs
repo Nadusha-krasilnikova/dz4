86b7518c4b57ad4429b08937ca3c2eca83acc70e
Nadusha <nadusha230275@gmail.com>
2022-07-04 11:00:42 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// дано число из отрезка(10,99).Показать наибольшую цифру числа
+int a=new Random( ).Next(10,99);
+Console.WriteLine(a);
+int resultB=(a/10) % 10;
+int resultC=a % 10;
+int max=resultB;
+if (resultB>max) max=resultB;
+if (resultC>max) max=resultC;
+Console.Write("максимальная цифра числа=");
+Console.Write(max);
